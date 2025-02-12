58bd700f5361571161f4492b2d3d772a8d8cc59f
cosmicsami <cosmicsami@hotmail.com>
2025-02-12 18:58:39 +0000

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+print("hello new app!!!")
+dictionary={
+    "name":"sam", 
+    "age":22,
+    "marks":100.0
+}
+print(dictionary)
+print(dictionary["name"])
+print(dictionary.get("name"))
+for i in dictionary:
+    print(i)
+for i in dictionary.values():
+     print(i)
